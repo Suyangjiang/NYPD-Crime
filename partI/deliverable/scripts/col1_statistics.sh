82b2341cc9e9f54a5ae2c25425be3623057df121
WEISU <jacketsu@hotmail.com>
2017-12-04 17:26:00 -0500

    update

--- partI/deliverable/scripts/col1_statistics.sh
+++ partI/deliverable/scripts/col1_statistics.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 /usr/bin/hadoop fs -rm -r col1_statistic_count.out
 /usr/bin/hadoop fs -rm -r col1_validation_count.out
 spark-submit col1.py /user/js9258/NYPD_Complaint_Data_Historic.csv
--- partI/deliverable/scripts/data_clean.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/hadoop fs -rm -r cleanedData.out
-spark-submit Data_Clean.py /user/js9258/NYPD_Complaint_Data_Historic.csv
-/usr/bin/hadoop fs -getmerge cleanedData.out cleanedData.out
