faf8fc3dc4a7dec98fbcc14c31c477cffb7d9a96
Suyangjiang <amberjiang93@outlook.com>
2017-12-16 21:35:48 -0500

    start hour count

---
+++ partII/deliverable/scripts/hour_amount.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import sys
+import re
+import string
+from pyspark import SparkContext
+from csv import reader
+from operator import add
+import time
+import datetime
+
+if __name__ == "__main__":
+	
+	sc = SparkContext()
+
+	lines = sc.textFile(sys.argv[1], 1)
+
+	# header = lines.first()
+	# Remove the header
+	lines = lines.mapPartitions(lambda x: reader(x))
+
+	lines = lines.map(lambda x: (x[2])).map(lambda s: datetime.datetime.strptime(s, '%H:%M:%S').time())
+	year = lines.map(lambda x: (x.hour, 1)).reduceByKey(lambda x, y: x + y).sortBy(lambda x: x[0]).sortBy(lambda x: x[1], False) 
+	year.collect()
+	
+	year.saveAsTextFile("hour_count.out")
+
+	# Collect the statistics
+	#statistic_count(lines)
+
+	sc.stop()
